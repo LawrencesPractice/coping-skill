447002a82d9699eaefa2394e43f30e4723298694
“Lawrence <tbby28@gmail.com>
2023-12-01 15:12:33 +1100

    added animation

--- src/components/explore/mindfulIntro/mindfulIntro.js
+++ src/components/explore/mindfulIntro/mindfulIntro.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import styled from 'styled-components';
-import mindful from '../../../assets/mindful.png';
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import mindful from "../../../assets/mindful.png";
 
 const ImageContainer = styled.div`
   position: relative;
@@ -29,20 +29,56 @@ const TextContainer = styled.div`
 const Title = styled.h1`
   font-size: 36px;
   margin-bottom: 20px;
-  color: #77DD77`;
+  color: #77dd77;
+`;
 
 const Description = styled.p`
   font-size: 18px;
-  color:  #AEC6CF;
+  color: #aec6cf;
 `;
 
+const TypingAnimation = ({ fullText }) => {
+  const [text, setText] = useState("");
+  const [charIndex, setCharIndex] = useState(0);
+
+  useEffect(() => {
+    const type = () => {
+      setText(fullText.slice(0, charIndex));
+      setCharIndex((prevCharIndex) => prevCharIndex + 1);
+    };
+
+    if (charIndex <= fullText.length) {
+      const typingTimeout = setTimeout(type, 100);
+      return () => clearTimeout(typingTimeout);
+    }
+  }, [charIndex, fullText]);
+
+  return <Title>{text}</Title>;
+};
+
 const MindfulIntroComponent = () => {
+  const [showDescription, setShowDescription] = useState(false);
+  const fullText = "Welcome to Mindful";
+  const descriptionText = "Discover inner peace and tranquility";
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setShowDescription(true);
+    }, fullText.length * 100 + 500); // Wait for fullText typing animation to finish and add a delay of 500 milliseconds
+
+    return () => clearTimeout(timeout);
+  }, []);
+
   return (
     <>
       <ImageContainer>
         <TextContainer>
-          <Title>Welcome to Mindful</Title>
-          <Description>Discover inner peace and tranquility</Description>
+          <TypingAnimation fullText={fullText} />
+          {showDescription && (
+            <Description>
+              <TypingAnimation fullText={descriptionText} />
+            </Description>
+          )}
         </TextContainer>
       </ImageContainer>
     </>
